27f720b5a1925efd370cc528e135927dcf300056
rafael.moura1 <rafael.moura1@estudante.ufla.br>
2022-05-10 15:27:06 -0300

    🔨

--- aws/templates/sqs+fargate/src/methods.ts
+++ aws/templates/sqs+fargate/src/methods.ts
@@ -3,13 +3,16 @@ import Receiver from './services/receiver'
 import env from './constants/env'
 import { ISqs } from './interfaces/ISqs';
 
+
 export const fn_fargate = async () => {
     const work = new Work()
     const receiver = new Receiver(env.SQS_URL, work.processor.bind(work))
     const params: ISqs.ReceiveMessage = {
         VisibilityTimeout: env.SCALE_UP_COOL_DOWN
     }
-    const sleep_time = env.SQS_MESSAGE_VISIBILITY_TIMEOUT * 1.5;
+    const millisecondsInSeconds = 1000;
+    const increaseTimeRate = 1.5;
+    const sleep_time = env.SQS_MESSAGE_VISIBILITY_TIMEOUT * millisecondsInSeconds * increaseTimeRate;
 
     await receiver.continuosReceiveAndProcessing({ params, sleep_time })
 }
