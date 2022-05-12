07651f322313a00d0224f9103100cb6f6cdeaec0
rafael.moura <rafael.moura@iclubs.com.br>
2022-05-12 17:41:19 -0300

    🔥 Remove table work

--- aws/templates/sqs+fargate/src/constants/env.ts
+++ aws/templates/sqs+fargate/src/constants/env.ts
@@ -3,7 +3,6 @@ const { env } = process
 export default {
   REGION: env.REGION,
   SQS_URL: env.SQS_URL,
-  POC_TABLE: env.POC_TABLE ? env.POC_TABLE : 'fargate',
   SQS_MESSAGE_VISIBILITY_TIMEOUT: Number(env.SQS_MESSAGE_VISIBILITY_TIMEOUT),
   SCALE_UP_COOL_DOWN: Number(env.SCALE_UP_COOL_DOWN)
 }
